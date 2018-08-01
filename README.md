68010d455172565d6cabf06553031ffafe18d3e8
yuanliwei <891789592@qq.com>
2018-08-02 01:07:38 +0800

    代码优化，添加列表假数据

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # fast-list
 
 显示大量数据列表的工具类，使用view回收重用方式实现。
+
+## 使用示例
+```javascript
+
+// 初始化列表
+let dom = document.querySelector('#list')
+var list = new FastList(dom)
+list.setHandler({
+  createViewHolder: (type)=>{
+    let view = $(`<div class="pt-1 col-12"><div class="alert alert-success" role="alert">
+    <b></b> <span></span> </div></div>`)
+    return {
+      dom: view[0],
+      num: view.find('b'),
+      name: view.find('span')
+    }
+  },
+  bindData: (type, position, holder, data)=>{
+    holder.num.text(data.index)
+    holder.name.text(data.name)
+  }
+})
+
+// 测试数据
+var datas = []
+for (var i = 0; i < 100000; i++) {
+  datas.push({
+    type: 'item',
+    index: i,
+    name: 'name:'+i
+  })
+}
+
+// setDatas
+list.setDatas(datas)
+```
--- js/FastList.js
+++ js/FastList.js
@@ -1,22 +1,30 @@
 class FastList {
-  constructor(dom) {
-    // 列表容器
-    this.dom =dom
-    // 每条列表项的最大计划高度，用于计算第一个item的位置和索引
-    this.maxItemHeight = 500
-    // 列表可见区域的高度
-    this.viewPortHeight = $(window).height()
-    // 顶部占位View
-    this.topView = document.createElement('div')
-    // 底部占位View
-    this.bottomView = document.createElement('div')
-    this.dom.append(this.topView)
-    this.dom.append(this.bottomView)
-    this.avgHeight = 0
-    this.heightMap = {}
+  constructor(root) {
+    this.root = root
+    this.root.style.position = 'relative'
+    this.root.style.overflowY = 'auto'
+    this.maxItemHeight = 100
+    this.anchor = document.createElement('div')
+    this.anchor.style.height='1px'
+    this.anchor.style.width='1px'
+    this.anchor.style.position='absolute'
+    this.anchor.style.transform='translateY(0px)'
+    this.root.append(this.anchor)
     this.datas = []
-    this.holders = []
-    FastList.instances.push(this)
+    this.holders = {}
+    this.cacheHolders = {}
+    this.updateTime = 0
+
+    this.root.onscroll = ()=>{
+      if (Date.now() - this.updateTime < 300) {
+        clearTimeout(this.timer)
+        this.timer = setTimeout(()=>{
+          this.scroll();
+        }, 100)
+      } else {
+        this.scroll();
+      }
+    }
   }
 
   setHandler(handler){
@@ -24,7 +32,10 @@ class FastList {
   }
 
   setDatas(datas){
-    this.datas = datas
+    this.datas = datas || []
+    this.anchor.style.transform=`translateY(${this.datas.length*this.maxItemHeight}px)`
+    this.holderMap = {}
+    this.recycleViews(1,-1)
     this.notifyAll()
   }
 
@@ -32,127 +43,59 @@ class FastList {
     this.scroll()
   }
 
-  getTop(){
-    return this.topView.clientHeight || 0
-  }
-
-  getHeight(){
-    return this.dom.clientHeight || 0
-  }
-
-  getViewPortHeight(){
-    this.dom.parent
-  }
-
-  getAvgHeight(){
-    let index = 0
-    let totalHeight = 0
-    let {heightMap, datas} = this
-    for (var k in heightMap) {
-      let v = heightMap[k]
-      index++
-      totalHeight += v
-    }
-    let avgHeight = parseInt(totalHeight / index)
-    return avgHeight
-  }
-
-  getPositionAndIndex(scrollTop){
-    let index = 0
-    let totalHeight = 0
-    let {heightMap, datas} = this
-    for (var k in heightMap) {
-      let v = heightMap[k]
-      if (totalHeight+v>=scrollTop) {
-        return {position:totalHeight, index:index}
-      }
-      index++
-      totalHeight += v
-    }
-    if (totalHeight==0) {
-      return {position: 0, index: 0};
-    }
-    let avgHeight = parseInt(totalHeight / index)
-    index = parseInt(scrollTop / avgHeight)
-    if (index>datas.length - 1) {
-      index = datas.length - 1
-    }
-    totalHeight = avgHeight * index;
-    return {position: totalHeight, index: index};
-  }
-
-  getTopPosition(scrollTop){
-    if (scrollTop == 0) {
-      return 0;
-    }
-    let index = 0
-    let totalHeight = 0
-    let {heightMap, datas} = this
-    for (var k in heightMap) {
-      console.log('k = '+k);
-      let v = heightMap[k] ||
-      if (totalHeight+v>=scrollTop) {
-        return totalHeight
-      }
-      index++
-      totalHeight += v
-    }
-  }
-
   getHolder(type){
-    let holder = this.handler.createViewHolder(type)
-    this.dom.append(holder.dom)
-    this.dom.append(this.bottomView)
+    let holders = this.cacheHolders[type]
+    if (!holders) { holders = this.cacheHolders[type] = [] }
+    let holder = holders.pop()
+    if (!holder) {
+      holder = this.handler.createViewHolder(type)
+      holder.dom.style.position='absolute'
+      holder.dom.style.transform=`translateY(0px)`
+      this.root.append(holder.dom)
+    } else {
+      holder.dom.style.opacity=1
+    }
+    holders = this.holders[type]
+    if (!holders) { holders = this.holders[type] = [] }
+    holders.push(holder)
     return holder
   }
 
-  bindData(type, index, holder, data){
-    this.handler.bindData(type, index, holder, data)
-  }
-
-  getBottom(holder){
-    let dom = holder.dom
-    console.log('dom bottom:'+(dom.offsetTop + dom.clientHeight));
-    return dom.offsetTop + dom.clientHeight
-  }
-
-  scroll(event){
-    let {topView, bottomView, datas, holders} = this
-    let windowHeight = $(window).height();
-    let scrollTop = $(window).scrollTop();
-    let topPosition = this.getTopPosition(scrollTop)
-    let topIndex = this.getTopIndex(topPosition)
-
-    // var {position, index} = this.getPositionAndIndex(scrollTop)
-    topView.style.height = topPosition + 'px'
-    var i = topIndex
-    for (; i < datas.length; i++) {
-      let holder = holders[i-topIndex]
-      let data = datas[i]
-      if (!holder) {
-        holder = this.getHolder(data.type)
+  recycleViews(top, bottom){
+    for (var type in this.holders) {
+      this.holders[type] = this.holders[type].filter((holder)=>{
+        let pos = parseInt(holder.dom.style.transform.match(/\d+/))
+        let holders = this.cacheHolders[type]
+        if (!holders) { holders = this.cacheHolders[type] = [] }
         holders.push(holder)
-      }
-      // holder.top = topPosition
-      this.bindData(data.type, i, holder, data)
-      this.heightMap[i] = holder.dom.clientHeight
-      topPosition += holder.dom.clientHeight
-      if (this.getBottom(holder) > windowHeight) {
+        holder.dom.style.opacity=0
+        return false
+      })
+    }
+  }
+
+  scroll(){
+    this.updateTime = Date.now()
+    let topPosition = parseInt(this.root.scrollTop / this.maxItemHeight) * this.maxItemHeight
+    let breakHeight = this.maxItemHeight + this.root.clientHeight
+    this.recycleViews(topPosition, breakHeight)
+    let countHeight = 0
+    let index = parseInt(this.root.scrollTop / this.maxItemHeight)
+    let domNum = 0
+    for (; index < this.datas.length; index++, domNum++) {
+      let data = this.datas[index]
+      let holder = this.getHolder(data.type)
+      this.handler.bindData(data.type, index, holder, data)
+      holder.dom.style.transform=`translateY(${topPosition+countHeight}px)`
+      countHeight += holder.dom.clientHeight
+      if (countHeight>= breakHeight) {
+        domNum++
         break
       }
     }
-    // bottomView.top = topPosition
-    bottomView.style.height = (datas.length - i) * this.getAvgHeight() + 'px'
+    if (countHeight) {
+      this.maxItemHeight = countHeight / domNum || 100
+      this.anchor.style.transform=`translateY(${this.datas.length*this.maxItemHeight}px)`
+    }
   }
-
 }
-
-FastList.instances = [];
-FastList.scroll = function (event) {
-  FastList.instances.forEach(function (instance) {
-    instance.scroll(event);
-  });
-};
-$(window).scroll(function(event){
-  FastList.scroll(event);
-});
