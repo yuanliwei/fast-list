5243c81ef823e550a79ab56fdd99cbd803ec9674
yuanliwei <891789592@qq.com>
2023-11-19 14:07:06 +0800

    update list implementation

--- js/FastList.js
+++ js/FastList.js
@@ -1,125 +1,213 @@
-class FastList {
-  constructor(root) {
-    this.root = root
-    this.root.style.position = 'relative'
-    this.root.style.overflowY = 'auto'
-    this.maxItemHeight = 100
-    this.anchor = document.createElement('div')
-    this.anchor.style.height='1px'
-    this.anchor.style.width='1px'
-    this.anchor.style.position='absolute'
-    this.anchor.style.transform='translateY(0px)'
-    this.root.append(this.anchor)
-    this.datas = []
-    this.holders = {}
-    this.cacheHolders = {}
-    this.updateTime = 0
+import { ListState } from "./lib.js"
 
-    this.root.onscroll = ()=>{
-      if (Date.now() - this.updateTime < 30) {
-        clearTimeout(this.timer)
-        this.timer = setTimeout(()=>{
-          this.scroll();
-        }, 20)
-      } else {
-        this.scroll();
-      }
-    }
+/**
+ * @typedef {{dom:HTMLElement}&Record<object,object>} Holder
+ */
+
+export class FastList {
+  /**
+   * @param {HTMLElement} list
+   * @param {number} scrollTop
+   */
+  constructor(list, scrollTop) {
+    this.list = list
+    this.scrollTop = scrollTop || 0
+    this.listState = new ListState(null)
+    this.list.style.position = 'relative'
+    this.list.style.overflowY = 'auto'
+    this.scroller = document.createElement('span')
+    this.scroller.style.display = 'block'
+    this.scroller.style.width = '10px'
+    this.list.prepend(this.scroller)
+    this.datas = []
+    this.size = 0
+    /** @type{Record<string|number,Holder[]>} */
+    this.holders = {}
+    /** @type{Record<string|number,Holder[]>} */
+    this.cacheHolders = {}
+
+    this.listState.updateListClientHeight(list.clientHeight)
+
+    this.listState.init(this.list, this.scrollTop, (o) => {
+      this.renderData = o.renderData
+      this.renderStart = o.renderStart
+      this.renderOffsets = o.renderOffsets
+      this.scrollBarHeight = o.scrollBarHeight
+      this.scrollTop = o.scrollTop
+      this.renderList()
+    })
+
+    this.resizeObserver = new ResizeObserver(() => {
+      this.listState.updateListClientHeight(list.clientHeight)
+    })
+    this.resizeObserver.observe(this.list)
+
+    this.list.addEventListener("wheel", (e) => { this.onWheel(e) }, { passive: false })
+    this.list.addEventListener("touchstart", (e) => { this.onTouchStart(e) }, { passive: true })
+    this.list.addEventListener("touchmove", (e) => { this.onTouchMove(e) }, { passive: false })
+    this.list.addEventListener("touchend", (e) => { this.onTouchEnd(e) })
+    this.list.addEventListener("scroll", (e) => { this.onScroll() })
   }
 
-  setHandler(handler){
+  destroy() {
+    this.resizeObserver.unobserve(this.list)
+    this.list.removeEventListener("wheel", this.onWheel)
+    this.list.removeEventListener("touchstart", this.onTouchStart)
+    this.list.removeEventListener("touchmove", this.onTouchMove)
+    this.list.removeEventListener("touchend", this.onTouchEnd)
+    this.list.removeEventListener("scroll", this.onScroll)
+  }
+
+
+  /**
+   * @param {TouchEvent} e
+   */
+  onTouchStart(e) {
+    this.listState.onTouchStart(e)
+  }
+
+  /**
+   * @param {TouchEvent} e
+   */
+  onTouchMove(e) {
+    this.listState.onTouchMove(e)
+  }
+
+  /**
+   * @param {TouchEvent} e
+   */
+  onTouchEnd(e) {
+    this.listState.onTouchEnd(e)
+  }
+
+  /**
+   * @param {WheelEvent} e
+   */
+  onWheel(e) {
+    this.listState.onWheel(e)
+  }
+
+  onScroll() {
+    this.listState.onScroll()
+  }
+
+  /**
+   * @param {number} scrollTop
+   * @param {boolean} [animation]
+   */
+  async scrollToWithListScrollTop(scrollTop, animation) {
+    await this.listState.scrollToWithListScrollTop(scrollTop, animation)
+  }
+
+  /**
+   * @param {number} offset
+   * @param {boolean} animation
+   */
+  async scrollListOffset(offset, animation) {
+    await this.listState.scrollListOffset(offset, animation)
+  }
+
+  /**
+   * @param {number} percent 0-1
+   * @param {boolean} [animation]
+   */
+  async scrollToPercent(percent, animation) {
+    await this.listState.scrollToPercent(percent, animation)
+  }
+
+  /**
+   * @param {number} index
+   * @param {boolean} [animation]
+   */
+  async scrollToPosition(index, animation) {
+    await this.listState.scrollToPosition(index, animation)
+  }
+
+  async reset() {
+    await this.listState.reset()
+  }
+
+  /**
+   * @param {any[]} data
+   */
+  updateListData(data) {
+    this.listState.updateListData(data)
+  }
+  /**
+   * @param {number} size
+   */
+  updateListDataSize(size) {
+    this.listState.updateListDataSize(size)
+  }
+  /**
+   * @param {number} listClientHeight
+   */
+  updateListClientHeight(listClientHeight) {
+    this.listState.updateListClientHeight(listClientHeight)
+  }
+
+  /**
+   * @param {{createViewHolder: (type:string|number)=>Holder,bindData:(type:string|number,position:number,holder:Holder,data:object)=>void}} handler
+   */
+  setHandler(handler) {
     this.handler = handler
   }
 
-  setDatas(datas){
+  /**
+   * @param {any[]} datas
+   */
+  setDatas(datas) {
     this.datas = datas || []
-    this.anchor.style.transform=`translateY(${this.datas.length*this.maxItemHeight}px)`
-    this.holderMap = {}
-    this.notifyAll()
-    this.setUpNoData()
+    this.updateListData(this.datas)
   }
 
-  setUpNoData(){
-    if (this.datas.length > 0) {
-      if (this.noDataView) {
-        this.noDataView.remove()
-      }
-    } else {
-      if (!this.noDataView) {
-        let createNoDataView = ()=>{
-          let h1 = document.createElement('h1')
-          h1.innerText = 'NO DATA!'
-          h1.classList.add('text-info')
-          return h1
-        }
-        this.noDataView = this.handler.createNoDataView && this.handler.createNoDataView() || createNoDataView()
-      }
-      this.root.prepend(this.noDataView)
-    }
+  notifyAll() {
+    this.renderList()
   }
 
-  notifyAll(){
-    this.scroll()
-  }
-
-  getHolder(type){
+  /**
+   * @param {string | number} type
+   */
+  getHolder(type) {
     let holders = this.cacheHolders[type]
     if (!holders) { holders = this.cacheHolders[type] = [] }
     let holder = holders.pop()
     if (!holder) {
       holder = this.handler.createViewHolder(type)
-      holder.dom.style.position='absolute'
-      holder.dom.style.transform=`translateY(0px)`
-      this.root.append(holder.dom)
+      holder.dom.style.position = 'absolute'
+      holder.dom.style.width = `100%`
+      this.list.append(holder.dom)
     } else {
-      holder.dom.style.opacity=1
+      holder.dom.style.opacity = '1'
     }
     holders = this.holders[type]
     if (!holders) { holders = this.holders[type] = [] }
     holders.push(holder)
+    holder.dom.parentElement.append(holder.dom)
     return holder
   }
 
-  recycleViews(){
+  recycleViews() {
     for (var type in this.holders) {
-      this.holders[type] = this.holders[type].filter((holder)=>{
-        let pos = parseInt(holder.dom.style.transform.match(/\d+/))
+      this.holders[type] = this.holders[type].filter((holder) => {
         let holders = this.cacheHolders[type]
         if (!holders) { holders = this.cacheHolders[type] = [] }
         holders.push(holder)
-        holder.dom.style.opacity=0
+        holder.dom.style.opacity = '0'
         return false
       })
     }
   }
 
-  scroll(){
-    this.updateTime = Date.now()
-    let topPosition = parseInt(this.root.scrollTop / this.maxItemHeight) * this.maxItemHeight
-    let breakHeight = this.maxItemHeight + this.root.clientHeight
+  renderList() {
     this.recycleViews()
-    let countHeight = 0
-    let index = parseInt(this.root.scrollTop / this.maxItemHeight)
-    let domNum = 0
-    if (index > this.datas.length) {
-      this.root.scrollTop = this.maxItemHeight*this.datas.length-breakHeight
-    }
-    for (; index < this.datas.length; index++, domNum++) {
-      let data = this.datas[index]
+    this.scroller.style.height = this.scrollBarHeight + 'px'
+    for (let i = 0; i < this.renderData.length; i++) {
+      const data = this.renderData[i]
       let holder = this.getHolder(data.type)
-      this.handler.bindData(data.type, index, holder, data)
-      holder.dom.style.transform=`translateY(${topPosition+countHeight}px)`
-      countHeight += holder.dom.clientHeight
-      if (countHeight>= breakHeight) {
-        domNum++
-        break
-      }
-    }
-    if (countHeight) {
-      this.maxItemHeight = countHeight / domNum || 100
-      this.anchor.style.transform=`translateY(${this.datas.length*this.maxItemHeight}px)`
+      this.handler.bindData(data.type, i + this.renderStart, holder, data)
+      holder.dom.style.top = this.renderOffsets[i] + 'px'
     }
   }
 }
 
-module.exports = FastList
---
+++ js/lib.js
@@ -0,0 +1,389 @@
+export class ListState {
+    /**
+     * @param {() => Promise<void>} tick
+     */
+    constructor(tick) {
+        /** @type{Map<number,number>} */
+        this.sizeMap = new Map()
+        this.listClientHeight = 0
+        this.listScrollTop = 0
+        this.listPageSize = 0
+        this.itemOffsetList = []
+        this.listRenderStart = 0
+        this.scrollBarHeight = 0
+        this.listContentHeight = 0
+        this.scrollStateVersion = 0
+
+        this.lastTouchY = 0
+        this.lastTouchTime = 0
+        this.touchSpeed = 0
+        this.flingSpeed = 0
+
+        this.data = []
+        this.dataSize = 0
+        this.renderData = []
+
+        this.tick = tick || (() => new Promise((resolve) => setTimeout(resolve)))
+    }
+
+    /**
+     * @param {HTMLElement} list
+     * @param {number} scrollTop
+     * @param {{(data:{renderData:any[];renderOffsets:number[];scrollBarHeight:number;renderStart:number;scrollTop:number;}):void;}} callback
+     */
+    async init(list, scrollTop, callback) {
+        this.list = list
+        this.listScrollTop = scrollTop
+        this.hasInit = true
+        this.onUpdate = callback
+        this.refresh()
+    }
+
+    async refresh() {
+        if (this.hasInit) {
+            await this.renderList(this.listScrollTop, this.data)
+        }
+    }
+
+    /**
+     * @param {number} height
+     */
+    updateListClientHeight(height) {
+        this.listClientHeight = height
+        this.refresh()
+        this.scrollToWithListScrollTop(this.listScrollTop, false)
+    }
+
+    /**
+     * @param {any[]} data
+     */
+    updateListData(data) {
+        this.data = data
+        this.dataSize = data.length
+        this.refresh()
+    }
+
+    /**
+     * @param {number} size
+     */
+    updateListDataSize(size) {
+        this.dataSize = Math.max(this.dataSize, size)
+        this.refresh()
+    }
+
+    /**
+     * @param {number} scrollTop
+     * @param {boolean} [animation]
+     */
+    async scrollToWithListScrollTop(scrollTop, animation) {
+        let diff = scrollTop - this.listScrollTop
+        let version = ++this.scrollStateVersion
+        if (animation) {
+            let delta = (scrollTop - this.listScrollTop) * 0.2
+            let changed = true
+            while (changed && this.scrollStateVersion == version) {
+                await animationFrame()
+                changed = this.updateListScrollTop(delta)
+                delta = (scrollTop - this.listScrollTop) * 0.2
+            }
+        } else {
+            this.updateListScrollTop(diff)
+        }
+    }
+
+    /**
+     * @param {number} offset
+     * @param {boolean} animation
+     */
+    async scrollListOffset(offset, animation) {
+        let countListScrollTop = offset + this.listScrollTop
+        await this.scrollToWithListScrollTop(countListScrollTop, animation)
+    }
+
+    /**
+     * @param {number} percent 0-1
+     * @param {boolean} [animation]
+     */
+    async scrollToPercent(percent, animation) {
+        let countListScrollTop = percent * (this.listContentHeight - this.list.clientHeight)
+        await this.scrollToWithListScrollTop(countListScrollTop, animation)
+    }
+
+    /**
+     * @param {number} index
+     * @param {boolean} [animation]
+     */
+    async scrollToPosition(index, animation) {
+        let countListScrollTop = countListContentHeightWithIndex(this.sizeMap, index)
+        await this.scrollToWithListScrollTop(countListScrollTop, animation)
+    }
+
+    async reset() {
+        this.sizeMap.clear()
+        await this.refresh()
+    }
+
+    /**
+     * @param {TouchEvent} e
+     */
+    onTouchStart(e) {
+        this.lastTouchY = e.touches[0].clientY
+        this.lastTouchTime = e.timeStamp
+        this.touchSpeed = 0
+        this.scrollStateVersion++
+    }
+
+    /**
+     * @param {TouchEvent} e
+     */
+    onTouchMove(e) {
+        let y = e.touches[0].clientY
+        let deltaY = this.lastTouchY - y
+        this.touchSpeed = deltaY / (e.timeStamp - this.lastTouchTime)
+        this.lastTouchY = y
+        this.lastTouchTime = e.timeStamp
+        if (this.updateListScrollTop(deltaY)) {
+            e.preventDefault()
+        }
+    }
+
+
+    /**
+     * @param {TouchEvent} e
+     */
+    async onTouchEnd(e) {
+
+        if (this.flingSpeed * this.touchSpeed < 0 || Math.abs(this.touchSpeed) < 3) {
+            this.flingSpeed = this.touchSpeed
+        } else {
+            this.flingSpeed += this.touchSpeed
+        }
+
+        if (Math.abs(this.flingSpeed) > 10) {
+            this.flingSpeed *= 3
+        }
+
+        let time = performance.now()
+        let speedDelta = 0.99
+        let version = ++this.scrollStateVersion
+        let changed = true
+        while (changed && version == this.scrollStateVersion) {
+            await animationFrame()
+            changed = this.updateListScrollTop(this.flingSpeed * (performance.now() - time))
+            time = performance.now()
+            this.flingSpeed *= speedDelta
+        }
+    }
+
+    /**
+     * @param {WheelEvent} e
+     */
+    onWheel(e) {
+        if (e.ctrlKey && e.cancelable) {
+            return
+        }
+        this.scrollStateVersion++
+        if (this.updateListScrollTop(e.deltaY)) {
+            e.preventDefault()
+        }
+    }
+
+    onScroll() {
+        if (this.skipOnScrollEventOnce) {
+            this.skipOnScrollEventOnce = false
+            return
+        }
+        let percent = this.list.scrollTop / (this.scrollBarHeight - this.list.clientHeight)
+        let countListScrollTop = percent * (this.listContentHeight - this.list.clientHeight)
+        if (Math.abs(countListScrollTop - this.listScrollTop) > 1) {
+            this.scrollStateVersion++
+            this.updateListScrollTop(countListScrollTop - this.listScrollTop)
+        }
+    }
+
+    /**
+     * @param {number} deltaY
+     * @returns {boolean} true if listScrollTop changed
+     */
+    updateListScrollTop(deltaY) {
+        if (!this.list) {
+            return false
+        }
+
+        let countListScrollTop = this.listScrollTop + deltaY
+
+        if (countListScrollTop < 0) {
+            countListScrollTop = 0
+        }
+
+        if (countListScrollTop > this.listContentHeight - this.listClientHeight) {
+            countListScrollTop = this.listContentHeight - this.listClientHeight
+        }
+
+        if (this.dataSize < this.listPageSize) {
+            countListScrollTop = 0
+        }
+
+        if (Math.abs(countListScrollTop - this.listScrollTop) > 0.01) {
+            this.listScrollTop = countListScrollTop
+            this.renderList(this.listScrollTop, this.data)
+
+            return true
+        } else {
+            return false
+        }
+    }
+
+    /**
+    * @param {number} scrollTop
+    * @param {object[]} [datas]
+    */
+    async renderList(scrollTop, datas) {
+        let o = countOffsetAndStartIndex(this.sizeMap, this.dataSize, this.listClientHeight, scrollTop)
+        this.scrollBarHeight = Math.min(this.listContentHeight, 100000)
+        let percent = scrollTop / (this.listContentHeight - this.listClientHeight)
+        let countScrollTop = percent * (this.scrollBarHeight - this.listClientHeight)
+
+        o.offsets = o.offsets.map(o => o + countScrollTop)
+
+        this.listRenderStart = o.startIndex
+        this.itemOffsetList = o.offsets
+        this.listPageSize = this.itemOffsetList.length
+        let end = this.listRenderStart + this.itemOffsetList.length
+        if (this.data.length < this.dataSize) {
+            this.renderData = Array.from({ length: this.itemOffsetList.length })
+        } else {
+            this.renderData = datas.slice(this.listRenderStart, end)
+        }
+
+        this.onUpdate({
+            renderData: this.renderData,
+            renderStart: this.listRenderStart,
+            renderOffsets: this.itemOffsetList,
+            scrollBarHeight: this.scrollBarHeight,
+            scrollTop: scrollTop,
+        })
+
+        await this.tick()
+
+        if (datas.length == 0) {
+            return
+        }
+
+        if (Math.abs(countScrollTop - this.list.scrollTop) > 0.01) {
+            this.skipOnScrollEventOnce = true
+            this.list.scrollTop = countScrollTop
+        }
+
+        let heightMapChange = false
+
+        let nodes = [...this.list.childNodes].filter((o) => o.nodeName == "DIV")
+        let previousNodeHeight = 0
+        let entries = [...this.sizeMap.entries()].sort((l, h) => l[0] - h[0])
+        for (let i = 0; i < entries.length; i++) {
+            const [index, value] = entries[i]
+            if (index <= this.listRenderStart) {
+                previousNodeHeight = value
+            } else {
+                break
+            }
+        }
+        for (let i = 0; i < this.renderData.length && i < nodes.length; i++) {
+            /** @type{*} */
+            let node = nodes[i]
+            /** @type{number} */
+            let nodeHeight = node.clientHeight
+            if (nodeHeight != previousNodeHeight && nodeHeight > 0) {
+                previousNodeHeight = nodeHeight
+                let index = this.listRenderStart + i
+                if (this.sizeMap.get(index) != nodeHeight) {
+                    this.sizeMap.set(index, nodeHeight)
+                    heightMapChange = true
+                }
+            }
+        }
+        if (heightMapChange) {
+            this.listContentHeight = countListContentHeightWithIndex(this.sizeMap, this.dataSize)
+            await this.renderList(scrollTop, datas)
+        }
+    }
+
+}
+
+/**
+ * @param {Map<number, number>} map
+ * @param {number} index
+ */
+function countListContentHeightWithIndex(map, index) {
+    let entries = [...map.entries()].sort((l, h) => l[0] - h[0])
+    let listContentHeight = 0
+    let previousIndex = 0
+    let previousValue = 0
+    for (let i = 0; i < entries.length; i++) {
+        const [indexKey, value] = entries[i]
+        if (indexKey > index) {
+            listContentHeight += (index - previousIndex) * previousValue
+            return listContentHeight
+        }
+        listContentHeight += (indexKey - previousIndex) * previousValue
+        previousIndex = indexKey
+        previousValue = value
+    }
+    listContentHeight += (index - previousIndex) * previousValue
+    return listContentHeight
+}
+
+/**
+ * @param {Map<number, number>} map
+ * @param {number} size
+ * @param {number} listClientHeight
+ * @param {number} scrollTop
+ */
+export function countOffsetAndStartIndex(map, size, listClientHeight, scrollTop) {
+
+    let offsets = []
+    let startIndex = 0
+
+    let entries = [...map.entries()].sort((l, h) => l[0] - h[0])
+
+    let listContentHeight = 0
+    let listItemOffset = 0
+
+    let previousIndex = 0
+    let previousValue = 0
+    for (let i = 0; i < entries.length; i++) {
+        const [index, value] = entries[i]
+        let contentHeight = listContentHeight + (index - previousIndex) * previousValue
+        if (contentHeight > scrollTop) {
+            break
+        }
+        listContentHeight = contentHeight
+        previousIndex = index
+        previousValue = value
+    }
+
+    if (previousValue == 0) {
+        offsets = [0]
+        startIndex = 0
+        return { startIndex, offsets }
+    }
+
+    startIndex = Math.floor((scrollTop - listContentHeight) / previousValue) + previousIndex
+    listItemOffset = (listContentHeight - scrollTop) % previousValue
+
+    offsets.push(listItemOffset)
+    let itemIndex = startIndex
+    let currentListBottom = listItemOffset + previousValue
+    while (++itemIndex < size && currentListBottom < listClientHeight) {
+        offsets.push(currentListBottom)
+        let height = previousValue
+        if (map.has(itemIndex)) {
+            height = previousValue = map.get(itemIndex)
+        }
+        currentListBottom += height
+    }
+
+    return { startIndex, offsets }
+}
+
+const animationFrame = () => new Promise((resolve) => requestAnimationFrame(resolve))
